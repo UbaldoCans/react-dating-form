0b976f1d5c6ff1adf2fdc4a1b79c091709e88f88
Ubaldo <morganfrezze22@gmail.com>
2022-08-18 15:05:12 -0500

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header(props){
     return(
         
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto uppercase"> 
-                seguimiento  pacientes {''}
+                seguimiento de pacientes {''}
                 <span className="text-indigo-400">veterinaria</span>
             </h1>
         
